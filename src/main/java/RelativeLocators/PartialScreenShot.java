0d726490b4c142bb5119b5f131c40422e00b8d49
Anvesh <anveshpothireddy@gmail.com>
2024-09-11 02:08:23 +0530

    This is commit by another me

--- src/main/java/RelativeLocators/PartialScreenShot.java
+++ src/main/java/RelativeLocators/PartialScreenShot.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PartialScreenShot {
+	
+	public void GitDemo() {
+		System.out.println("Demo");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
